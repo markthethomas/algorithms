dd1c9b181d7fbde980a884dcb393cc3d93e65875
Mark Thomas <markthethomas@gmail.com>
2015-11-28 22:58:40 -0800

    Cleaning things up a bit — still not solving problem though

--- python/selectionSort.py
+++ python/selectionSort.py
@@ -1,7 +1,5 @@
 # Selection Sort
 
-import unittest
-
 # find smallest, sawp with first element
 # find second smallest, swap with second element
 # find nth smallest, swap w/ n
@@ -14,10 +12,6 @@ def selectionSort(sequence):
     Input: unsorted sequence {a1, a2, a3, ... an}
     Output: permutation of the input sequence such that {a1 <= a2 <= a3 ... an}
     """
-    def swap(first, second, array):
-        temp = first
-        array[first] = array[second]
-        array[second] = temp
 
     for i in list(range(len(sequence) - 1)):
         # Set the min to initial item
@@ -25,21 +19,13 @@ def selectionSort(sequence):
 
         # Check the rest to see if anything is smaller
         for j in list(range(len(sequence))):
-            if sequence[j] < sequence[smallest]:
-                # TODO assign new min here!
+            if sequence[j] <= sequence[smallest]:
+                smallest = j
+        print(sequence)
+        temp = sequence[smallest]
+        sequence[smallest] = sequence[i]
+        sequence[i] = temp
 
-    return sequence
-
-example_input = list(range(4))
-
-print(selectionSort(example_input))
-
-
-class TestSelectionSort(unittest.TestCase):
-    """ TestSelectionSort"""
-    def test_sorting(self):
-        unsorted_array = [2, 4, 1, 3]
-        self.assertEqual(selectionSort(unsorted_array), [1, 2, 3, 4])
-
-if __name__ == '__main__':
-    unittest.main()
+example_input = [2, 1, 3, 4]
+selectionSort(example_input)
+print(example_input)
