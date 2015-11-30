d72eb62bc0afe1b37c675babed8373bd536de73c
Mark Thomas <markthethomas@gmail.com>
2015-11-30 14:41:59 -0800

    Create way to compute ratios of each number type

--- python/challenges/plusMinus.py
+++ python/challenges/plusMinus.py
@@ -1,3 +1,5 @@
+import unittest
+
 """
 Problem Statement:
 Given an array of integers, calculate which fraction of the elements are positive, negative, and zeroes, respectively. Print the decimal value of each fraction.
@@ -8,4 +10,40 @@ The second line contains N space-separated integers describing the array of numb
 
 Output Format:
 Print each value on its own line with the fraction of positive numbers first, negative numbers second, and zeroes third.
+
+There are 3 positive numbers, 2 negative numbers, and 1 zero in the array.
+The fraction of the positive numbers, negative numbers and zeroes are 36=0.500000, 26=0.333333 and 16=0.166667, respectively.
 """
+
+
+def plusMinus(arr):
+
+    def roundToPrecision(num):
+        return round(num / n, 6)
+
+    n = len(arr)
+    pos, neg, zer = 0, 0, 0
+    for item in arr:
+        if item == 0:
+            zer += 1
+        elif item > 0:
+            pos += 1
+        elif item < 0:
+            neg += 1
+
+    results = []
+
+    for result in [pos, neg, zer]:
+        results.append(roundToPrecision(result))
+
+    return results
+
+
+class TestPlusMinus(unittest.TestCase):
+
+    def test_plus_minus(self):
+        arr = [-4, 3, -9, 0, 4, 1]
+        self.assertEqual(plusMinus(arr), [0.500000, 0.333333, 0.166667])
+
+if __name__ == '__main__':
+    unittest.main()
