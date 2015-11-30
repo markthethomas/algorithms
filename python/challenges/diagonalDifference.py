39dd435aede4915745b3878e1241015614ffc271
Mark Thomas <markthethomas@gmail.com>
2015-11-30 14:21:38 -0800

    Finish diagonal difference problem

--- python/challenges/diagonalDifference.py
+++ python/challenges/diagonalDifference.py
@@ -1,3 +1,4 @@
+import unittest
 # Given a square matrix of size N×N, calculate the absolute difference
 # between the sums of its diagonals.
 # 3 rows
@@ -5,6 +6,29 @@
 #            [4   5   6],
 #            [10  8 -12] ]
 
+
 def diagnonalDifference(matrix):
-    firstDiagonal = 0
-    secondDiagonal = 0
+    primaryDiagonal, secondaryDiagonal = 0, 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if i == j:
+                primaryDiagonal += matrix[i][j]
+    for k in range(len(matrix)):
+        length = len(matrix[k])
+        secondaryDiagonal += matrix[k][length - k - 1]
+
+    return abs(primaryDiagonal - secondaryDiagonal)
+
+
+class TestTimeConversion(unittest.TestCase):
+    """TestTimeConversion"""
+
+    def test_conversion(self):
+        sample_matrix = [[11,  2,  4],
+                         [4, 5,  6],
+                         [10, 8, -12]]
+        self.assertEqual(diagnonalDifference(sample_matrix), 15)
+
+if __name__ == '__main__':
+    unittest.main()
