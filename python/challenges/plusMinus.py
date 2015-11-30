7645d98247df22dbd4a5af19d89174d347d827e6
Mark Thomas <markthethomas@gmail.com>
2015-11-30 14:30:10 -0800

    Create main challenge file with proble statement and i/o expectations

---
+++ python/challenges/plusMinus.py
@@ -0,0 +1,11 @@
+"""
+Problem Statement:
+Given an array of integers, calculate which fraction of the elements are positive, negative, and zeroes, respectively. Print the decimal value of each fraction.
+
+Input Format:
+The first line, N, is the size of the array.
+The second line contains N space-separated integers describing the array of numbers (A1,A2,A3,⋯,AN).
+
+Output Format:
+Print each value on its own line with the fraction of positive numbers first, negative numbers second, and zeroes third.
+"""
